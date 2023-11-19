25656e5dc4082ab5e3f4343711cd5176f4cf4827
Kevin Molina <kevinmolina.dev@gmail.com>
2023-11-19 18:32:47 -0400

    Implementacion de una interfaz, Agregando mas contexto al modelo

--- src/models/Voice/text_to_voice.py
+++ src/models/Voice/text_to_voice.py
@@ -1,16 +1,7 @@
-from .variants.default import Default
-from .variants.standard import Standard
-
-
 class TextToVoice:
     def __init__(self,text,type):
         self.text=text
         self.type=type
 
     def speak(self):
-        types_voice={
-            "default":Default(),
-            "standard":Standard()
-        }
-        builder=types_voice.get(self.type)
-        builder.speak(self.text)
+        self.type.speak(self.text)
--- src/models/nlp.py
+++ src/models/nlp.py
@@ -1,28 +1,23 @@
 from .Predictive.predictive import Predictive
 from .Text.voice_to_text import VoiceToText
 from .Voice.text_to_voice import TextToVoice
-
+from .Voice.variants import controller
 class NLP:
-    def init_listener(self):
+    def init_listener():
         voice = VoiceToText()
         return voice.init_listener()
 
     def process_question(self,question,context):
-        text = ""
-        if(question):
-            predictive = Predictive("mrm8488/distill-bert-base-spanish-wwm-cased-finetuned-spa-squad2-es").get_model()
-            data = predictive({'question': question,'context':context})
-            # Ordena los resultados por start en orden ascendente y end en orden descendente
-            best_results = sorted(data, key=lambda x: (x["start"], -x["end"]))
-            text = best_results[0]["answer"]
-            """ for item in best_results:
-                text+=f"{item['answer']} | {item['start']} | {item['end']}\n-------------------\n" """
-        else:
-            text="Por favor ingrese una pregunta :D"
+        text=""
+        predictive = Predictive("mrm8488/distill-bert-base-spanish-wwm-cased-finetuned-spa-squad2-es").get_model()
+        data = predictive({'question': question,'context':context})
+        # Sacamos la diferencia entre start y end y ordenamos en base a la direnrecia mas alta
+        best_result = max(data, key=lambda x: x["end"] - x["start"])
+        text+=f"{best_result['answer']}"
         return text
     
     def say_result(self,text):
-        handle = TextToVoice(text,"default")
+        handle = TextToVoice(text,controller.default)
         handle.speak()
            
 
